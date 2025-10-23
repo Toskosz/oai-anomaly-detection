99c351a813288b5d6e3bf8b6a1f00e1d7367af7d
Tokarski <thiago.tokarski2001@gmail.com>
2025-10-23 20:44:25 -0300

    fix traffic generation

--- generate-ue-traffic/generate-ue2.py
+++ generate-ue-traffic/generate-ue2.py
@@ -1,16 +1,17 @@
 import pandas as pd
+from scapy.layers.inet import *
 from scapy.all import *
 import time
 import random
-from scapy.contrib.gtp import GTP_U_Header, GTPEchoRequest, GTPEchoResponse
+from scapy.contrib.gtp import GTP_U_Header
 
 
 # Load data with the same column names as before
-column_names = ["duration", "protocol_type", "service", "flag", "src_bytes", "dst_bytes", "land", "wrong_fragment", "urgent", "hot", "num_failed_logins", "logged_in", "num_compromised", "root_shell", "su_attempted", "num_root", "num_file_creations", "num_shells", "num_access_files", "num_outbound_cmds", "is_host_login", "is_guest_login", "count", "srv_count", "serror_rate", "srv_serror_rate", "rerror_rate", "srv_rerror_rate", "same_srv_rate", "diff_srv_rate", "srv_diff_host_rate", "dst_host_count", "d>
+column_names = ["duration", "protocol_type", "service", "flag", "src_bytes", "dst_bytes", "land", "wrong_fragment", "urgent", "hot", "num_failed_logins", "logged_in", "num_compromised", "root_shell", "su_attempted", "num_root", "num_file_creations", "num_shells", "num_access_files", "num_outbound_cmds", "is_host_login", "is_guest_login", "count", "srv_count", "serror_rate", "srv_serror_rate", "rerror_rate", "srv_rerror_rate", "same_srv_rate", "diff_srv_rate", "srv_diff_host_rate", "dst_host_count", "dst_host_srv_count", "dst_host_same_srv_rate", "dst_host_diff_srv_rate", "dst_host_same_src_port_rate", "dst_host_srv_diff_host_rate", "dst_host_serror_rate", "dst_host_srv_serror_rate", "dst_host_rerror_rate", "dst_host_srv_rerror_rate", "label", "difficulty_level"]
 test_df = pd.read_csv('Test.txt', header=None, names=column_names)
 
-def generate_packet(row):
-    max_payload_size = 1400
+def generate_packet(row, orientation, size):
+
     service_ports = {
         'http': 80, 'https': 443, 'ftp': 21, 'ftp_data': 20, 'smtp': 25, 'pop_3': 110,
         'telnet': 23, 'imap4': 143, 'ssh': 22, 'domain': 53, 'gopher': 70,
@@ -42,40 +43,66 @@ def generate_packet(row):
     protocol = row['protocol_type'].lower()
     service = row['service']
     flag = flag_mapping.get(row['flag'], '')
-    src_bytes = row['src_bytes']
-    dst_bytes = row['dst_bytes']
     
     port = service_ports.get(service, 0)
-    src_payload = 'X' * min(src_bytes, max_payload_size)
+    payload = 'X' * size
+
+    src_ip = "12.1.1.130"
+    dst_ip = "192.168.70.145"
+
+    if orientation == 1:
+        src_ip = "12.1.1.130"
+        dst_ip = "192.168.70.145"
 
     if protocol == 'tcp':
-        inner_packet = IP(src= "12.1.1.130", dst="192.168.70.145")/TCP(dport=port, flags=flag)/Raw(load=src_payload)
+        inner_packet = IP(src=src_ip, dst=dst_ip)/TCP(dport=port, flags=flag)/Raw(load=payload)
     elif protocol == 'udp':
-        inner_packet = IP(src= "12.1.1.130", dst="192.168.70.145")/UDP(dport=port)/Raw(load=src_payload)
+        inner_packet = IP(src=src_ip, dst=dst_ip)/UDP(dport=port)/Raw(load=payload)
     else:
-        inner_packet = IP(src= "12.1.1.130", dst="192.168.70.145")/ICMP()  # Default for other protocols
-    
+        inner_packet = IP(src=src_ip, dst=dst_ip)/ICMP()  # Default for other protocols
+
     # Encapsulate in GTP-U packet
-    #gtp_packet = IP(src= "12.1.1.130", dst="12.1.1.129")/UDP(sport=2152, dport=2152)/GTP_U_Header(teid=1)/inner_packet
+    # gtp_packet = IP(src=src_ip, dst=dst_ip)/UDP(sport=2152, dport=2152)/GTP_U_Header(teid=1)/inner_packet
 
-    # Handling response packet if there is any payload to respond with
-    if dst_bytes > 0:
-        dst_payload = 'X' * min(dst_bytes, max_payload_size)
-        response_inner_packet = IP(src="192.168.70.145", dst="12.1.1.130")/TCP(sport=port, dport=1024, flags='PA')/Raw(load=dst_payload)
-        # Encapsulate response in GTP-U
-        #response_gtp_packet = IP(src= "12.1.1.130", dst="12.1.1.129")/UDP(sport=2152, dport=2152)/GTP_U_Header(teid=1)/response_inner_packet
-        return (inner_packet, response_inner_packet)
-    else:
-        return (inner_packet, None)
+    return inner_packet
 
-def simulate_traffic(dataframe):
-    for index, row in dataframe.iterrows():
-        packet, response_packet = generate_packet(row)
-        print(f"Sending GTP encapsulated packet with size: {len(bytes(packet))}")
+def simulate_traffic(flow):
+    remaining_src_bytes = flow["src_bytes"] - 300
+    remaining_dst_bytes = flow["dst_bytes"]
+
+    # first packet is always from UE
+    first_packet = generate_packet(flow, 0, 300)
+    send(first_packet, iface="oaitun_ue1")
+    
+    while remaining_dst_bytes != 0 or remaining_src_bytes != 0:
+        # 0 = src -> dst
+        # 1 = response (dst -> src)
+        packet_orientation = random.randint(0, 1)
+        packet_size = 0
+
+        while True:
+            packet_size = random.randint(200, 1400)
+            if packet_orientation == 0:
+                if packet_size <= remaining_src_bytes:
+                    remaining_src_bytes -= packet_size
+                    break
+                if remaining_src_bytes < 200:
+                    packet_size = remaining_src_bytes 
+                    remaining_src_bytes = 0
+                    break
+            else:
+                if packet_size <= remaining_dst_bytes:
+                    remaining_dst_bytes -= packet_size
+                    break
+                if remaining_dst_bytes < 200:
+                    packet_size = remaining_dst_bytes 
+                    remaining_dst_bytes = 0
+                    break
+
+        packet = generate_packet(flow, packet_orientation, packet_size)
+        print(f"Sending GTP encapsulated packet with size: {len(bytes(packet_size))}")
         send(packet, iface="oaitun_ue1")  # Sending the main packet on the correct interface
-        if response_packet:
-            print(f"Sending GTP encapsulated response packet with size: {len(bytes(response_packet))}")
-            send(response_packet, iface="oaitun_ue1")
-        time.sleep(random.uniform(0.1, 1.0))  # Simulated traffic timing
 
-simulate_traffic(test_df)
+random_row = test_df.sample(1).iloc[0]
+
+simulate_traffic(random_row)
